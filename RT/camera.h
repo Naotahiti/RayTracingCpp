016d16a96af631bb4f884854acc598d1c2d28f6c
Naotahiti <87338610+Naotahiti@users.noreply.github.com>
2026-06-27 02:48:22 +0200

    camera update

--- RT/camera.h
+++ RT/camera.h
@@ -12,7 +12,11 @@ public:
     int    image_width = 100;  // Rendered image width in pixel count
     int    samples_per_pixel = 10;   // Count of random samples for each pixel
     int    max_depth = 10;   // Maximum number of ray bounces into scene
+
     double vfov = 90;  // Vertical view angle (field of view)
+    point3 lookfrom = point3(0, 0, 0);   // Point camera is looking from
+    point3 lookat = point3(0, 0, -1);  // Point camera is looking at
+    vec3   vup = vec3(0, 1, 0);     // Camera-relative "up" direction
 
     void render(const hittable& world) {
         initialize();
